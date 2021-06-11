1a485b6b1368c6d7b9e6028ae1ed6d5ca0dd3d30
vivaan90890 <75307495+vivaan90890@users.noreply.github.com>
2021-06-11 16:56:38 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,88 @@
+function btn1(){
+window.location="exersize1.html"
+}
+function btn2(){
+window.location="exersize2.html"
+}
+function btn3(){
+window.location="exersize3.html"
+}
+function btn4(){
+window.location="exersize4.html"
+}
+function btn5(){
+window.location="exersize5.html"
+}
+function btn6(){
+window.location="exersize6.html"
+}
+function btn7(){
+window.location="exersize8.html"
+}
+function btn9(){
+    var synth = window.speechSynthesis;
+
+    speak_data = "good job";
+
+    var utterThis = new SpeechSynthesisUtterance(speak_data);
+
+    synth.speak(utterThis);
+}
+function btn10(){
+    window.location="index.html"
+    }
+    function btn11(){
+        var synth = window.speechSynthesis;
+    
+        speak_data = "blink your eyes and repeat it for 1 minute";
+    
+        var utterThis = new SpeechSynthesisUtterance(speak_data);
+    
+        synth.speak(utterThis);
+    }
+    function btn12(){
+        var synth = window.speechSynthesis;
+    
+        speak_data = "Move your eyes up, down, right, left, clockwise and anticlockwise. repeat it 5 times";
+    
+        var utterThis = new SpeechSynthesisUtterance(speak_data);
+    
+        synth.speak(utterThis);
+    }
+    function btn13(){
+        var synth = window.speechSynthesis;
+    
+        speak_data = "For gazing into darkness put your elbows on the table. close your eyes and cup them with your palms. relax and gaze into darkness. hold this position for couple of minutes";
+    
+        var utterThis = new SpeechSynthesisUtterance(speak_data);
+    
+        synth.speak(utterThis);
+    }
+    function btn14(){
+        var synth = window.speechSynthesis;
+    
+        speak_data = "eye massage one. close your eyes. then squeeze your eyes tightly shut for two to three seconds. relax the muscles around your eyes. squeeze your eyes shut again. repeat ten times. eye massage two. close your eyes. gently massage your eyelids  with cirular motions. press lightly. don't try to press too hard. make ten circular movements, first colockwise, then counter clockwise";
+    
+        var utterThis = new SpeechSynthesisUtterance(speak_data);
+    
+        synth.speak(utterThis);
+    }
+    function btn15(){
+        var synth = window.speechSynthesis;
+    
+        speak_data = "for eye concentration. hold a pencil at an arm's length and focus it. slowly bring it closer to you nose. move it farther from your vision until you can no longer keep it on focus. repeat about 10 times a day ";
+    
+        var utterThis = new SpeechSynthesisUtterance(speak_data);
+    
+        synth.speak(utterThis);
+    }
+    function btn16(){
+        var synth = window.speechSynthesis;
+    
+        speak_data = "Healthy Eye Tips first. Eat a balanced diet cantaining vitamin A and C and green leafy vegitables second. Do eye exercises regularly to improve blood circulation, strengthen eye muscles and to enhance vision third. Take goodnight and sound sleep to support health of eyes fourth. Take breaks while using digital devices fifth. Wash your hands and face thoroughly to avoid germs spreading into eyes.        ";
+    
+        var utterThis = new SpeechSynthesisUtterance(speak_data);
+    
+        synth.speak(utterThis);
+    }
+      
